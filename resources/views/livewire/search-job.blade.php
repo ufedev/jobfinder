f675490319deb1167bdb62e6a6c95022f154363f
Federico Malfasi <federicomalfasi@Mac-mini-de-Federico.local>
2024-03-29 10:49:42 -0300

    Finished, fix corrections

--- resources/views/livewire/search-job.blade.php
+++ resources/views/livewire/search-job.blade.php
@@ -3,7 +3,7 @@
     <form class='w-full flex flex-col md:flex-row items-center gap-2' wire:submit='search_job'>
 
         <div
-            class='flex-1 w-[95%] flex flex-col md:grid md:grid-cols-3 gap-3 items-center text-black dark:text-gray-500 '>
+            class='flex-1 w-[95%] flex flex-col md:grid md:grid-cols-3 gap-2 items-center text-black dark:text-gray-500 '>
             <x-text-input class='py-1 w-full' placeholder='Busqueda' wire:model='search' />
             <select class='flex-1 w-full dark:bg-gray-900 rounded-lg p-1' wire:model='category'>
                 <option value='0' selected>Todas las Categorías</option>
@@ -19,6 +19,8 @@
             </select>
         </div>
 
-        <x-primary-button class="max-w-[95%] md:w-3/12 mt-0">Buscar</x-primary-button>
+        <input type='submit'
+            class="max-w-[95%] md:w-3/12 px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+            value='Buscar' />
     </form>{{-- Nothing in the world is as soft and yielding as water. --}}
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <div class='bg-slate-50 dark:bg-slate-950'>
         <section class='w-[95%] md:w-6/12 lg:w-5/12 mx-auto py-16 text-center space-y-5'>
-            <h2 class="text-7xl text-slate-800 dark:text-slate-300 font-extrabold">Encontrá trabajos remotos de
+            <h2 class="text-5xl md:text-7xl text-slate-800 dark:text-slate-300 font-extrabold ">Encontrá trabajos
+                remotos de
                 desarrollador</h2>
             <p class='text-slate-700 dark:text-slate-400 text-xl'>Encontra el trabajo que deseas, desarrollador frontend,
                 backend, mobile y
